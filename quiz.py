d77b6542047eec37f0e2a621f9cf45e97200b284
cam531 <cameronleong@outlook.com>
2022-09-24 18:40:33 -0700

    Finishing the quiz

--- quiz.py
+++ quiz.py
@@ -27,26 +27,75 @@ if q2 == "there":
 else:
     print("Incorrect!")
 
-q3 = input("Question 3: ")
+q3 = input("Question 3: Convert this sentence into past tense: “I am going to read a book at school today.")
+if q3 == "I read a book at school today.":
+    print("Correct!")
+    score += 1
+    grammar += 1
+else:
+    print("Incorrect!")
+
+q4 = input("Question 4: I want to (where/wear/ware) this new outfit tomorrow")
+if q4 == "wear":
+    print("Correct!")
+    score += 1
+    spelling += 1
+else:
+    print("Incorrect!")
+
+q5 = input("Question 5: Hannah and Harold (jumped/ arrived/ flew) at the bus station early.")
+if q5 == "arrived":
+    print("Correct!")
+    score += 1
+    vocab += 1
+else:
+    print("Incorrect!")
+
+q6 = input("Question 6: That student (wrote/ ate/ slept) the best essay in the class last month.")
+if q6 == "wrote":
+    print("Correct!")
+    score += 1
+    vocab += 1
+else:
+    print("Incorrect!")
+
+q7 = input("Question 7: My favourite animal is a (table, dog, chips)")
+if q7 == "dog":
+    print("Correct!")
+    score += 1
+    vocab += 1
+else:
+    print("Incorrect!")
+
+q8 = input("Question 8: If only you believe you can (achieve/ acheive/ acheeve)!")
+if q8 == "achieve":
+    print("Correct!")
+    score += 1
+    spelling += 1
+else:
+    print("Incorrect!")
+
+q9 = input("Question 9: I like to eat (dessert/ desert/ desertt)")
+if q9 == "dessert":
+    print("Correct!")
+    score += 1
+    spelling += 1
+else:
+    print("Incorrect!")
+
+print("Congrats on finishing your quiz! Your score is " + str(score) + "/9!")
+
+scores = [vocab, spelling, grammar]
+scores.sort()
+
+if scores[0] == scores[1] and scores[1] == scores[2]:
+    print("You should practice all three topics!")
+elif vocab == scores[0]:
+    print("You should practice your vocabulary choice!")
+elif spelling == scores[0]:
+    print("You should practice your spelling!")
+else:
+    print("You should practice your grammar!")
 
 
-q4 = input("Question 4: ")
-
-
-q5 = input("Question 5: ")
-
-
-q6 = input("Question 6: ")
-
-
-q7 = input("Question 7: ")
-
-
-q8 = input("Question 8: ")
-
-
-q9 = input("Question 9: ")
-
-
-q10 = input("Question 10: ")
 
